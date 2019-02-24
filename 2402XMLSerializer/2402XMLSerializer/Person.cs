2b6b3e790f4c094194c99319d9d35b72cf3f477a
dotnet31102018 <44984976+dotnet31102018@users.noreply.github.com>
2019-02-24 19:54:23 +0200

    Add files via upload

---
+++ 2402XMLSerializer/2402XMLSerializer/Person.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2402XMLSerializer
+{
+    public class Person
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        // parametersless ctor for serialization
+        public Person()
+        {
+
+        }
+
+        public Person(string firstName, string lastName)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+        }
+    }
+}
---
+++ 2402XMLSerializer/2402XMLSerializer/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace _2402XMLSerializer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            Person p1 = new Person("Dannaa", "Shovevany");
+
+            XmlSerializer myXmlSerializer = new XmlSerializer(typeof(Person));
+
+            using (Stream file = new FileStream(@"c:\temp\xmlfile.xml", FileMode.Create)) // creating new file stream
+            {
+                myXmlSerializer.Serialize(file, p1);
+
+            } //closing file stream
+            */
+
+            Person p2;
+
+            XmlSerializer myXmlSerializer = new XmlSerializer(typeof(Person));
+
+            using (Stream file = new FileStream(@"c:\temp\xmlfile.xml", FileMode.Open)) // creating new file stream
+            {
+                p2 = myXmlSerializer.Deserialize(file) as Person;
+
+            } //closing file stream
+            Console.WriteLine();
+        }
+    }
+}
