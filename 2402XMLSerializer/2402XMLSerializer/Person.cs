0152e1a3fddc43bf795a6af25c1f1193bfb5e856
dotnet31102018 <44984976+dotnet31102018@users.noreply.github.com>
2019-02-24 20:59:09 +0200

    Add files via upload

--- 2402XMLSerializer/2402XMLSerializer/Person.cs
+++ 2402XMLSerializer/2402XMLSerializer/Person.cs
@@ -3,13 +3,27 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Serialization;
 
 namespace _2402XMLSerializer
 {
+    [XmlRoot("SpecialPerson")]
     public class Person
     {
+        [XmlElement("PrivateName")]
         public string FirstName { get; set; }
         public string LastName { get; set; }
+
+        private int code = 1;
+        protected string neighebrhood = "ofakim";
+        internal int cars = 4;
+        public static int numberStatic = 4;
+
+        [XmlIgnore]
+        public int Age = 5;
+
+        [XmlAttribute]
+        public string IAMAttribute = "hello";
 
         // parametersless ctor for serialization
         public Person()
--- 2402XMLSerializer/2402XMLSerializer/Program.cs
+++ 2402XMLSerializer/2402XMLSerializer/Program.cs
@@ -12,7 +12,7 @@ namespace _2402XMLSerializer
     {
         static void Main(string[] args)
         {
-            /*
+            
             Person p1 = new Person("Dannaa", "Shovevany");
 
             XmlSerializer myXmlSerializer = new XmlSerializer(typeof(Person));
@@ -22,18 +22,49 @@ namespace _2402XMLSerializer
                 myXmlSerializer.Serialize(file, p1);
 
             } //closing file stream
-            */
+            
 
             Person p2;
 
-            XmlSerializer myXmlSerializer = new XmlSerializer(typeof(Person));
+            //XmlSerializer myXmlSerializer = new XmlSerializer(typeof(Person));
 
             using (Stream file = new FileStream(@"c:\temp\xmlfile.xml", FileMode.Open)) // creating new file stream
             {
                 p2 = myXmlSerializer.Deserialize(file) as Person;
 
             } //closing file stream
+
+            /*
+            // --------------------- Array
+            Person[] pArray =
+            {
+                new Person("number 1", "the best!"),
+                new Person("zehavi", "python"),
+                new Person("bond", "java"),
+                new Person("james", "c#")
+
+            };
+
+            XmlSerializer myXmlSerializerArray = new XmlSerializer(typeof(Person[]));
+
+            using (Stream file = new FileStream(@"c:\temp\xmlarrayfile.xml", FileMode.Create)) // creating new file stream
+            {
+                myXmlSerializerArray.Serialize(file, pArray);
+
+            } //closing file stream
+            
+
+            Person[] p2Array;
+
+            using (Stream file = new FileStream(@"c:\temp\xmlarrayfile.xml", FileMode.Open)) // creating new file stream
+            {
+                p2Array = myXmlSerializerArray.Deserialize(file) as Person[];
+
+            } //closing file stream
+
+    */
             Console.WriteLine();
         }
     }
 }
+
